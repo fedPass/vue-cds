fe5654c8d78cf61ce71591bb990be0d3b196ac26
fedPass <federica.passanante@gmail.com>
2020-03-05 18:13:59 +0100

    iniziata struttura Home.vue

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Home!</div>
+    <!-- template può avere solo un figlio, al suo interno scrivo struttura html della mia app -->
+    <div>
+        <!--
+        - divido le parti della mia app in particelle e per ognuna creo un componente
+        - creo la cartella components e al suo interno un file .vue per ogni componente
+        - 
+        -->
+        <music-header></music-header>
+        <!-- il cds-container al suo interno avrà il singolo cd -->
+        <music-cds-container></music-cds-container>
+    </div>
 </template>
